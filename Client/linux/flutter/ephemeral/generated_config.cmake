42813cb7eec7fcbe3d5a97d5488771c0fabbd965
leuleyasu <leuleyasu7@gmail.com>
2023-07-12 00:09:35 +0300

    making auth ui ,making request

---
+++ Client/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/l/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/l/Documents/15_days_project/flutter_node_Eccomerse_project/Client" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/l/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/l/Documents/15_days_project/flutter_node_Eccomerse_project/Client"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yYTM0MDFjOWJiYjVhOWE5YWVjNzRkNGY3MzVkMThhOWRkM2ViZjJkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/l/Documents/15_days_project/flutter_node_Eccomerse_project/Client/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/l/Documents/15_days_project/flutter_node_Eccomerse_project/Client/lib/main.dart"
+)
